aea3d41f68f741f4319b916ef8fa8f06dc353c84
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-03-27 11:30:14 +0330

    solve contains duplicate problem

---
+++ Arrays_Hashing/ContainsDuplicate/ContainsDuplicateProblem.cs
@@ -0,0 +1,18 @@
+﻿namespace Arrays_Hashing.ContainsDuplicate
+{
+    public static class ContainsDuplicateProblem
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> seenNumbers = new(nums.Length);
+
+            foreach (int num in nums)
+            {
+                if (!seenNumbers.Add(num))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
