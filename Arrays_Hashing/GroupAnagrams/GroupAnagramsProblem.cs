10100f4ebdb4b3ee33643c45687d7d0e14cf6273
Saeed <saeed_prime@outlook.com>
2023-03-28 01:06:33 +0330

    solve group anagrams problem

---
+++ Arrays_Hashing/GroupAnagrams/GroupAnagramsProblem.cs
@@ -0,0 +1,27 @@
+﻿namespace Arrays_Hashing.GroupAnagrams
+{
+    public static class GroupAnagramsProblem
+    {
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, string[]> anagramHash = new();
+            string sortedStr;
+            foreach (var str in strs)
+            {
+                sortedStr = string.Concat(str.OrderBy(x => x));
+
+                if (anagramHash.ContainsKey(sortedStr))
+                {
+                    anagramHash[sortedStr] =
+                        anagramHash[sortedStr].Append(str).ToArray();
+                }
+                else
+                {
+                    anagramHash.Add(sortedStr, new string[] { str });
+                }
+            }
+
+            return anagramHash.Select(x => x.Value.ToArray()).ToArray();
+        }
+    }
+}
--- Arrays_Hashing/Program.cs
+++ Arrays_Hashing/Program.cs
@@ -1,5 +1,10 @@
-﻿using Arrays_Hashing.TwoSum;
+﻿using Arrays_Hashing.GroupAnagrams;
 
-var indexes = TwoSumProblem.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+var name = "daseed";
+name = string.Concat(name.OrderBy(x => x));
+
+//var indexes = TwoSumProblem.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+var groupAnagrams = GroupAnagramsProblem.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+
 
 Console.ReadLine();
