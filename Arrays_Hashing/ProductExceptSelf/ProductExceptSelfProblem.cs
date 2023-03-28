db0d1240c081dd7336807664234216d9e1c04f99
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-03-28 14:35:23 +0330

    create product except self problem class

---
+++ Arrays_Hashing/ProductExceptSelf/ProductExceptSelfProblem.cs
@@ -0,0 +1,11 @@
+﻿namespace Arrays_Hashing.ProductExceptSelf
+{
+    public static class ProductExceptSelfProblem
+    {
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+
+            return new int[] { nums[0] };
+        }
+    }
+}
