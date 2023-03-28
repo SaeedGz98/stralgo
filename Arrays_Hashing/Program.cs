7ff39b20d2f890c4eb0cfa89136413aba3900ffc
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-03-28 14:09:31 +0330

    solve k frequent element problem

--- Arrays_Hashing/Program.cs
+++ Arrays_Hashing/Program.cs
@@ -1,10 +1,6 @@
 ﻿using Arrays_Hashing.GroupAnagrams;
+using Arrays_Hashing.TopKFrequentElements;
 
-var name = "daseed";
-name = string.Concat(name.OrderBy(x => x));
-
-//var indexes = TwoSumProblem.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
-var groupAnagrams = GroupAnagramsProblem.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
-
+var result = TopKFrequentElementsProblem.TopKFrequent(new int[] { 1, 1, 1, 2, 2, 2, 3, 3, 3 }, 2);
 
 Console.ReadLine();
---
+++ Arrays_Hashing/TopKFrequentElements/TopKFrequentElementsProblem.cs
@@ -0,0 +1,42 @@
+﻿namespace Arrays_Hashing.TopKFrequentElements
+{
+    public static class TopKFrequentElementsProblem
+    {
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> dict = new();
+            List<int>[] freq = new List<int>[nums.Length + 1];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dict[nums[i]] = dict.GetValueOrDefault(nums[i], 0) + 1;
+            }
+
+            foreach (var repeat in dict)
+            {
+                if (freq[repeat.Value] != null)
+                    freq[repeat.Value].Add(repeat.Key);
+                else
+                    freq[repeat.Value] = new List<int> { repeat.Key };
+            }
+
+            List<int> result = new();
+
+            for (int i = nums.Length; i > 0; i--)
+            {
+                if (freq[i] == null)
+                    continue;
+
+                foreach (int num in freq[i])
+                {
+                    result.Add(num);
+
+                    if (result.Count == k)
+                        return result.ToArray();
+                }
+            }
+
+            return null;
+        }
+    }
+}
