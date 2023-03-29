f1e1320f93dd2788899d7a0b7094102d88c031da
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-03-29 13:01:04 +0330

    solve product except self problem

--- Arrays_Hashing/ProductExceptSelf/ProductExceptSelfProblem.cs
+++ Arrays_Hashing/ProductExceptSelf/ProductExceptSelfProblem.cs
@@ -4,8 +4,25 @@
     {
         public static int[] ProductExceptSelf(int[] nums)
         {
+            int[] res = Enumerable.Repeat(1, nums.Length).ToArray();
 
-            return new int[] { nums[0] };
+            int prefix = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res[i] = prefix;
+                prefix *= nums[i];
+            }
+
+            int postfix = 1;
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                res[i] = res[i] * postfix;
+                postfix *= nums[i];
+            }
+
+            return res;
         }
     }
 }
--- Arrays_Hashing/Program.cs
+++ Arrays_Hashing/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Arrays_Hashing.GroupAnagrams;
+using Arrays_Hashing.ProductExceptSelf;
 using Arrays_Hashing.TopKFrequentElements;
 
-var result = TopKFrequentElementsProblem.TopKFrequent(new int[] { 1, 1, 1, 2, 2, 2, 3, 3, 3 }, 2);
-
+var res = ProductExceptSelfProblem.ProductExceptSelf(new int[] { 1, 2, 3, 4 });
 Console.ReadLine();
