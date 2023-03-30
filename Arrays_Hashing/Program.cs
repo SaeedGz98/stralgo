22769951c4447e72cbca9886b1d1e4c8ca6bd998
Saeed <saeed_prime@outlook.com>
2023-03-30 09:15:57 +0330

    solve valid sodoku problem

--- Arrays_Hashing/Program.cs
+++ Arrays_Hashing/Program.cs
@@ -1,6 +1,16 @@
-﻿using Arrays_Hashing.GroupAnagrams;
-using Arrays_Hashing.ProductExceptSelf;
-using Arrays_Hashing.TopKFrequentElements;
+﻿using Arrays_Hashing.ValidSudoku;
 
-var res = ProductExceptSelfProblem.ProductExceptSelf(new int[] { 1, 2, 3, 4 });
+char[][] sudokuItems = new char[][] {
+    new char[] {'5','3','.','.','7','.','.','.','.'},
+    new char[] {'6','.','.','1','9','5','.','.','.'},
+    new char[] {'.','9','8','.','.','.','.','6','.'},
+    new char[] {'8','.','.','.','6','.','.','.','3'},
+    new char[] {'4','.','.','8','.','3','.','.','1'},
+    new char[] {'7','.','.','.','2','.','.','.','6'},
+    new char[] {'.','6','.','.','.','.','2','8','.'},
+    new char[] {'.','.','.','4','1','9','.','.','5'},
+    new char[] {'.','.','.','.','8','.','.','7','9'},
+};
+
+ValidSudokuProblem.IsValidSudoku(sudokuItems);
 Console.ReadLine();
---
+++ Arrays_Hashing/ValidSudoku/ValidSudokuProblem.cs
@@ -0,0 +1,41 @@
+﻿namespace Arrays_Hashing.ValidSudoku
+{
+    public static class ValidSudokuProblem
+    {
+        public static bool IsValidSudoku(char[][] board)
+        {
+            Dictionary<int, HashSet<int>> cols = new();
+            Dictionary<int, HashSet<int>> rows = new();
+            Dictionary<(int i, int j), HashSet<int>> squares = new();
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (!cols.ContainsKey(i))
+                    cols[i] = new HashSet<int>();
+
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i][j] == '.')
+                        continue;
+
+                    if (!rows.ContainsKey(j))
+                        rows[j] = new HashSet<int>();
+
+                    if (!squares.ContainsKey((i / 3, j / 3)))
+                        squares[(i / 3, j / 3)] = new HashSet<int>();
+
+                    if (!cols[i].Add(board[i][j]))
+                        return false;
+
+                    if (!rows[j].Add(board[i][j]))
+                        return false;
+
+                    if (!squares[(i / 3, j / 3)].Add(board[i][j]))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
