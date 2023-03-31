d151ec2c6940186c1f34eaea8a9d091a3239bb1f
Saeed <saeed_prime@outlook.com>
2023-03-31 14:38:10 +0330

    solve longest consecutive sequence problem

---
+++ Arrays_Hashing/LongestConsecutiveSequence/LongestConsecutiveSequenceProblem.cs
@@ -0,0 +1,30 @@
+﻿namespace Arrays_Hashing.LongestConsecutiveSequence
+{
+    public static class LongestConsecutiveSequenceProblem
+    {
+        public static int LongestConsecutive(int[] nums)
+        {
+            HashSet<int> numSet = nums.ToHashSet<int>();
+            int longest = 0;
+            int length;
+            int nextNum;
+
+            foreach (int num in nums)
+            {
+                if (numSet.Contains(num - 1)) continue;
+
+                length = 1;
+                nextNum = num + 1;
+                while (numSet.Contains(nextNum))
+                {
+                    length++;
+                    nextNum++;
+                }
+
+                longest = Math.Max(longest, length);
+            }
+
+            return longest;
+        }
+    }
+}
--- Arrays_Hashing/Program.cs
+++ Arrays_Hashing/Program.cs
@@ -1,16 +1,5 @@
-﻿using Arrays_Hashing.ValidSudoku;
+﻿using Arrays_Hashing.LongestConsecutiveSequence;
 
-char[][] sudokuItems = new char[][] {
-    new char[] {'5','3','.','.','7','.','.','.','.'},
-    new char[] {'6','.','.','1','9','5','.','.','.'},
-    new char[] {'.','9','8','.','.','.','.','6','.'},
-    new char[] {'8','.','.','.','6','.','.','.','3'},
-    new char[] {'4','.','.','8','.','3','.','.','1'},
-    new char[] {'7','.','.','.','2','.','.','.','6'},
-    new char[] {'.','6','.','.','.','.','2','8','.'},
-    new char[] {'.','.','.','4','1','9','.','.','5'},
-    new char[] {'.','.','.','.','8','.','.','7','9'},
-};
+LongestConsecutiveSequenceProblem.LongestConsecutive(new int[] { 100, 4, 200, 1, 3, 2 });
 
-ValidSudokuProblem.IsValidSudoku(sudokuItems);
 Console.ReadLine();
