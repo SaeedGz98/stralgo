a88b47c5fc92715cff01799ee39dadd866b58c21
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-03 17:09:33 +0330

    .

--- Stack/DailyTemperatures/DailyTemperaturesProblem.cs
+++ Stack/DailyTemperatures/DailyTemperaturesProblem.cs
@@ -4,7 +4,13 @@
     {
         public static int[] DailyTemperatures(int[] temperatures)
         {
+            int[] res = Enumerable.Repeat(0, temperatures.Length).ToArray();
+            Stack<(int value, int index)> stack = new();
 
+            foreach (int temp in temperatures)
+            {
+                while (stack.Count != 0 && stack.Peek)
+            }
         }
     }
 }
