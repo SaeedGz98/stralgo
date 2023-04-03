a87a7976ab10fa1431e549a1adbe2147c0319806
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-03 10:26:17 +0330

    create class for min stack

---
+++ Stack/MinStack/MinStackProblem.cs
@@ -0,0 +1,30 @@
+﻿namespace Stack.MinStack
+{
+    public class MinStackProblem
+    {
+        public MinStack()
+        {
+
+        }
+
+        public void Push(int val)
+        {
+
+        }
+
+        public void Pop()
+        {
+
+        }
+
+        public int Top()
+        {
+
+        }
+
+        public int GetMin()
+        {
+
+        }
+    }
+}
