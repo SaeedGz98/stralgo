2498e2fc416a84ce0264e051ef9f146ed1745f0e
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-03 15:45:00 +0330

    solve generate parenthesis problem

---
+++ Stack/GenerateParentheses/GenerateParenthesesProblem.cs
@@ -0,0 +1,42 @@
+﻿namespace Stack.GenerateParentheses
+{
+    public static class GenerateParenthesesProblem
+    {
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            Stack<char> stack = new();
+            List<string> result = new();
+
+            void Backtrack(int openN, int closedN)
+            {
+                if (openN == closedN && closedN == n)
+                {
+                    result.Add(string.Join(string.Empty, stack.Reverse()));
+                    return;
+                }
+
+                if (openN < n)
+                {
+                    stack.Push('(');
+
+                    Backtrack(openN + 1, closedN);
+
+                    stack.Pop();
+                }
+
+                if (closedN < openN)
+                {
+                    stack.Push(')');
+
+                    Backtrack(openN, closedN + 1);
+
+                    stack.Pop();
+                }
+            }
+
+            Backtrack(0, 0);
+
+            return result;
+        }
+    }
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Stack.EvaluateReversePolishNotation;
+using Stack.GenerateParentheses;
 
 //var result = EvaluateReversePolishNotationProblem.EvalRPN(new string[] { "2", "1", "+", "3", "*" });
 //var result = EvaluateReversePolishNotationProblem.EvalRPN(new string[] { "4", "13", "5", "/", "+" });
-var result = EvaluateReversePolishNotationProblem.EvalRPN(new string[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" });
+//var result = EvaluateReversePolishNotationProblem.EvalRPN(new string[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" });
+
+var result = GenerateParenthesesProblem.GenerateParenthesis(3);
 
 Console.WriteLine(result);
