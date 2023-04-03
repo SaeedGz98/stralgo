44bb76fc9f0e2b4c06daaa7a52397321a3f6388c
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-03 09:17:13 +0330

    solve valid parentheses problem

---
+++ Stack/ValidParentheses/ValidParenthesesProblem.cs
@@ -0,0 +1,41 @@
+﻿namespace Stack.ValidParentheses
+{
+    public static class ValidParenthesesProblem
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> chars = new();
+
+            foreach (var character in s)
+            {
+                if (!chars.Any())
+                {
+                    chars.Push(character);
+                    continue;
+                }
+
+                if (chars.Peek() == '(' && character == ')')
+                {
+                    chars.Pop();
+                    continue;
+                }
+
+                if (chars.Peek() == '{' && character == '}')
+                {
+                    chars.Pop();
+                    continue;
+                }
+
+                if (chars.Peek() == '[' && character == ']')
+                {
+                    chars.Pop();
+                    continue;
+                }
+
+                chars.Push(character);
+            }
+
+            return !chars.Any();
+        }
+    }
+}
