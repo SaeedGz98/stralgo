72475527c864971cb925f7b348a897b197c967a1
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-03 10:59:46 +0330

    min stack bug fix

--- Stack/MinStack/MinStackProblem.cs
+++ Stack/MinStack/MinStackProblem.cs
@@ -2,29 +2,48 @@
 {
     public class MinStackProblem
     {
-        public MinStack()
-        {
+        private Stack<int> _stack;
+        private Stack<int> _minStack;
 
+        public MinStackProblem()
+        {
+            _stack = new();
+            _minStack = new();
         }
 
         public void Push(int val)
         {
+            _stack.Push(val);
 
+            if (_minStack.Count == 0 || val <= _minStack.Peek())
+                _minStack.Push(val);
         }
 
         public void Pop()
         {
+            if (_stack.Count == 0)
+                return;
 
+            int poped = _stack.Pop();
+
+            if (_minStack.Peek() == poped)
+                _minStack.Pop();
         }
 
         public int Top()
         {
+            if (_stack.Count == 0)
+                return -1;
 
+            return _stack.Peek();
         }
 
         public int GetMin()
         {
+            if (_minStack.Count == 0)
+                return -1;
 
+            return _minStack.Peek();
         }
     }
 }
