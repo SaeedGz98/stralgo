655bb7ed1e9aaca4fbaf2e3fb53f78eff6b9a6ac
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-03 13:27:10 +0330

    solve evaluate reverse polish notation problem

---
+++ Stack/EvaluateReversePolishNotation/EvaluateReversePolishNotationProblem.cs
@@ -0,0 +1,34 @@
+﻿namespace Stack.EvaluateReversePolishNotation
+{
+    public static class EvaluateReversePolishNotationProblem
+    {
+        public static int EvalRPN(string[] tokens)
+        {
+            Stack<int> stack = new();
+
+            foreach (var token in tokens)
+            {
+                if (int.TryParse(token, out int num))
+                {
+                    stack.Push(num);
+                    continue;
+                }
+
+                int secondNum = stack.Pop();
+                int firstNum = stack.Pop();
+
+                int newAppende = token[0] switch
+                {
+                    '+' => firstNum + secondNum,
+                    '-' => firstNum - secondNum,
+                    '*' => firstNum * secondNum,
+                    '/' => firstNum / secondNum
+                };
+
+                stack.Push(newAppende);
+            }
+
+            return stack.Pop();
+        }
+    }
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Stack.EvaluateReversePolishNotation;
+
+//var result = EvaluateReversePolishNotationProblem.EvalRPN(new string[] { "2", "1", "+", "3", "*" });
+//var result = EvaluateReversePolishNotationProblem.EvalRPN(new string[] { "4", "13", "5", "/", "+" });
+var result = EvaluateReversePolishNotationProblem.EvalRPN(new string[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" });
+
+Console.WriteLine(result);
