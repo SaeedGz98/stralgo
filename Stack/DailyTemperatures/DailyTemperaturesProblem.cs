ec3a5f4f958ce56c0103d8996a7815b705642a24
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-03 15:48:54 +0330

    create daily temperatures problem class

---
+++ Stack/DailyTemperatures/DailyTemperaturesProblem.cs
@@ -0,0 +1,10 @@
+﻿namespace Stack.DailyTemperatures
+{
+    public static class DailyTemperaturesProblem
+    {
+        public static int[] DailyTemperatures(int[] temperatures)
+        {
+
+        }
+    }
+}
