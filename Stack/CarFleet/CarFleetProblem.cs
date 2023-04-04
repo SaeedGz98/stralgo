b7481d14ad523827cfa108c703fc2cfd3d420724
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-04 11:03:34 +0330

    car fleet uncompleted

---
+++ Stack/CarFleet/CarFleetProblem.cs
@@ -0,0 +1,10 @@
+﻿namespace Stack.CarFleet
+{
+    public static class CarFleetProblem
+    {
+        public static int CarFleet(int target, int[] position, int[] speed)
+        {
+            return target;
+        }
+    }
+}
