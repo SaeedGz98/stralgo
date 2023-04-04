c336d7aadcdfcb533ee4af0a678b4fd673b0995d
Saeed <saeed_prime@outlook.com>
2023-04-05 01:22:54 +0330

    c

--- Stack/LargestRectangleInHistogram/LargestRectangleInHistogramProblem.cs
+++ Stack/LargestRectangleInHistogram/LargestRectangleInHistogramProblem.cs
@@ -4,6 +4,21 @@
     {
         public static int LargestRectangleArea(int[] heights)
         {
+            Stack<(int index, int height)> stack = new();
+            int largestArea = 0;
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (stack.Count == 0 || stack.Peek().height <= heights[i])
+                {
+                    stack.Push((i, heights[i]));
+                }
+                else
+                {
+
+
+                }
+            }
         }
     }
 }
