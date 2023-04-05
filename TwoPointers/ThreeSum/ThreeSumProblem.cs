3f0405c3d9ae0afd66a0e5e06ed82c881852e569
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-05 14:00:23 +0330

    solve three sum problem

---
+++ TwoPointers/ThreeSum/ThreeSumProblem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoPointers.ThreeSum
+{
+    public static class ThreeSumProblem
+    {
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            ReadOnlySpan<int> span = nums.OrderBy(x => x).ToArray().AsSpan();
+            List<List<int>> res = new();
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                if (i > 0 && span[i] == span[i - 1])
+                    continue;
+
+                int left = i + 1;
+                int right = span.Length - 1;
+
+                while (left < right)
+                {
+                    int sumOfThree = span[i] + span[left] + span[right];
+
+                    if (sumOfThree > 0)
+                        right--;
+                    else if (sumOfThree < 0)
+                        left++;
+                    else
+                    {
+                        res.Add(new List<int> { span[i], span[left], span[right] });
+                        left++;
+                        while (span[left] == span[left - 1] && left < right)
+                            left++;
+                    }
+                }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
