2f0e66a9b7cbc2e0f44b926a661f1ad7a0b2317f
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-05 12:26:01 +0330

    use span in two sum ii

--- TwoPointers/TwoSumII/TwoSumIIProblem.cs
+++ TwoPointers/TwoSumII/TwoSumIIProblem.cs
@@ -1,14 +1,17 @@
-﻿namespace TwoPointers.TwoSumII
+﻿using System;
+
+namespace TwoPointers.TwoSumII
 {
     public static class TwoSumIIProblem
     {
         public static int[] TwoSum(int[] numbers, int target)
         {
-            int left = 0, right = numbers.Length - 1;
+            ReadOnlySpan<int> span = numbers.AsSpan();
+            int left = 0, right = span.Length - 1;
 
             while (left < right)
             {
-                int sum = numbers[left] + numbers[right];
+                int sum = span[left] + span[right];
 
                 if (sum < target)
                     left++;
