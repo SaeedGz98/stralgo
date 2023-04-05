1bf79ebb827bd702a1df41977c10d1ebe5e9f125
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-05 09:06:20 +0330

    refactor largest rectangle histogram problem

--- Stack/LargestRectangleInHistogram/LargestRectangleInHistogramProblem.cs
+++ Stack/LargestRectangleInHistogram/LargestRectangleInHistogramProblem.cs
@@ -9,22 +9,16 @@
 
             for (int i = 0; i < heights.Length; i++)
             {
-                if (stack.Count == 0 || stack.Peek().height <= heights[i])
-                {
-                    stack.Push((i, heights[i]));
-                }
-                else
-                {
-                    int fromIndex = i;
-                    while (stack.Count != 0 && heights[i] < stack.Peek().height)
-                    {
-                        (int index, int height) popped = stack.Pop();
-                        fromIndex = popped.index;
-                        maxArea = Math.Max(maxArea, (i - popped.index) * popped.height);
-                    }
+                int start = i;
 
-                    stack.Push((fromIndex, heights[i]));
+                while (stack.Count != 0 && heights[i] < stack.Peek().height)
+                {
+                    (int index, int height) popped = stack.Pop();
+                    start = popped.index;
+                    maxArea = Math.Max(maxArea, (i - popped.index) * popped.height);
                 }
+
+                stack.Push((start, heights[i]));
             }
 
             foreach (var remainingItems in stack)
