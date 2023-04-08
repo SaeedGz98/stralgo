f76fd7b151a07bf76c1890e36f99528c24082983
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-08 17:04:18 +0330

    solve time to buy stock

--- SlidingWindow/Program.cs
+++ SlidingWindow/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿using SlidingWindow.TimeToBuySellStock;
+
+TimeToBuySellStockProblem.MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
--- SlidingWindow/TimeToBuySellStock/TimeToBuySellStockProblem.cs
+++ SlidingWindow/TimeToBuySellStock/TimeToBuySellStockProblem.cs
@@ -1,10 +1,24 @@
-﻿namespace SlidingWindow.TimeToBuySellStock
+﻿using System;
+
+namespace SlidingWindow.TimeToBuySellStock
 {
     public static class TimeToBuySellStockProblem
     {
         public static int MaxProfit(int[] prices)
         {
+            int maxProfit = 0, left = 0, right = 1;
 
+            while (right < prices.Length)
+            {
+                if (prices[left] < prices[right])
+                    maxProfit = Math.Max(maxProfit, prices[right] - prices[left]);
+                else
+                    left = right;
+
+                right++;
+            }
+
+            return maxProfit;
         }
     }
 }
