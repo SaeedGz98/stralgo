360ac8eb0dfb8701c5b7791c19accd413206667a
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-09 10:50:38 +0330

    solve longest substring without repeating characters problem

---
+++ SlidingWindow/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersProblem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SlidingWindow.LongestSubstringWithoutRepeatingCharacters
+{
+    public static class LongestSubstringWithoutRepeatingCharactersProblem
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            HashSet<char> set = new();
+            int left = 0;
+            int maxSize = 0;
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                while (set.Contains(s[right]))
+                {
+                    set.Remove(s[left]);
+                    left++;
+                }
+
+                set.Add(s[right]);
+                maxSize = Math.Max(maxSize, right - left + 1);
+            }
+
+            return maxSize;
+        }
+    }
+}
