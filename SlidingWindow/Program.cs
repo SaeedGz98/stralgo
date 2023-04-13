dd40d9330fa9370b8d6a577dc3c9c8bf029b5314
Saeed <saeed_prime@outlook.com>
2023-04-13 19:44:28 +0330

    remove unusable codes

--- SlidingWindow/Program.cs
+++ SlidingWindow/Program.cs
@@ -1,6 +1,1 @@
-﻿using SlidingWindow.MinimumWindowSubstring;
-
-//MinimumWindowSubstringProblem.MinWindow("ADOBECODEBANC", "ABC");
-var result = MinimumWindowSubstringProblem.MinWindow("a", "b");
-
-var a = 2;
+﻿
