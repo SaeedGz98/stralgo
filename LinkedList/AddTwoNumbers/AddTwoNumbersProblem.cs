684d8233ba67d159d1e1c3e1ce4372338aedf7d5
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-25 17:06:47 +0330

    add two number problem

---
+++ LinkedList/AddTwoNumbers/AddTwoNumbersProblem.cs
@@ -0,0 +1,26 @@
+﻿namespace LinkedList.AddTwoNumbers
+{
+    public static class AddTwoNumbersProblem
+    {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode dummy = new ListNode();
+            ListNode tail = dummy;
+
+            int remain = 0;
+
+            while (l1.next != null && l2.next != null)
+            {
+                int sum = remain + l1.val + l2.val;
+                remain = sum % 10;
+
+                tail.next = new(remain);
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            return l1;
+        }
+    }
+}
