fba0a7e86b1e403ae8290e0518377c145d30d311
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-29 16:52:12 +0330

    search matrix

---
+++ BinarySearch/Search2DMatrix/Search2DMatrixProblem.cs
@@ -0,0 +1,13 @@
+﻿namespace BinarySearch.Search2DMatrix
+{
+    public static class Search2DMatrixProblem
+    {
+        public static bool SearchMatrix(int[][] matrix, int target)
+        {
+
+
+
+            return false;
+        }
+    }
+}
