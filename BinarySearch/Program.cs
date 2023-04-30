2c5dc918d15c158b7cfadb2ad20771d381a1ce5b
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-04-30 10:14:03 +0330

    solve search 2d matrix problem

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -1,7 +1,3 @@
-﻿
+﻿using BinarySearch.Search2DMatrix;
 
-using BinarySearch.BinarySearch;
-
-var binarySearchResult = BinarySearchProblem.Search(new int[] { 5 }, 5);
-
-Console.WriteLine(binarySearchResult);
+Search2DMatrixProblem.SearchMatrix(new int[][] { new int[] { 1, 3, 5, 7 }, new int[] { 10, 11, 16, 20 }, new int[] { 23, 30, 34, 60 } }, 16);
--- BinarySearch/Search2DMatrix/Search2DMatrixProblem.cs
+++ BinarySearch/Search2DMatrix/Search2DMatrixProblem.cs
@@ -4,8 +4,48 @@
     {
         public static bool SearchMatrix(int[][] matrix, int target)
         {
+            int rows = matrix.Length, cols = matrix[0].Length;
 
+            int top = 0, bottom = rows - 1;
+            int row = default;
 
+            while (top <= bottom)
+            {
+                row = (top + bottom) / 2;
+
+                if (target < matrix[row][0])
+                    bottom = row - 1;
+                else if (target > matrix[row][^1])
+                    top = row + 1;
+                else
+                    break;
+            }
+
+            if (top > bottom)
+                return false;
+
+            row = (top + bottom) / 2;
+
+            bool hasFound = BinarySearch(matrix[row], target);
+
+            return hasFound;
+        }
+
+        private static bool BinarySearch(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+
+                if (target > nums[middle])
+                    left = middle + 1;
+                else if (target < nums[middle])
+                    right = middle - 1;
+                else
+                    return true;
+            }
 
             return false;
         }
