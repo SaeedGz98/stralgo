2c4a113f033fccdaacd55cff4b2f7fc091b0c637
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-01 10:04:22 +0330

    solve search in rotated sorted array problem

--- BinarySearch/SearchInRotatedSortedArray/SearchInRotatedSortedArrayProblem.cs
+++ BinarySearch/SearchInRotatedSortedArray/SearchInRotatedSortedArrayProblem.cs
@@ -4,7 +4,32 @@
     {
         public static int Search(int[] nums, int target)
         {
+            int left = 0, right = nums.Length - 1;
 
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+
+                if (nums[middle] == target)
+                    return middle;
+
+                if (nums[left] <= nums[middle])
+                {
+                    if (target > nums[middle] || target < nums[left])
+                        left = middle + 1;
+                    else
+                        right = middle - 1;
+                }
+                else
+                {
+                    if (target < nums[middle] || target > nums[right])
+                        right = middle - 1;
+                    else
+                        left = middle + 1;
+                }
+            }
+
+            return -1;
         }
     }
 }
