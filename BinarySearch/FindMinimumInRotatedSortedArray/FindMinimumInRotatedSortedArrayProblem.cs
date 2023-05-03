07ae58a8f0b24eb53a1247c344c8c2091686a059
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-03 15:43:27 +0330

    solve find minimum rotated sorted array problem

--- BinarySearch/FindMinimumInRotatedSortedArray/FindMinimumInRotatedSortedArrayProblem.cs
+++ BinarySearch/FindMinimumInRotatedSortedArray/FindMinimumInRotatedSortedArrayProblem.cs
@@ -4,7 +4,28 @@
     {
         public static int FindMin(int[] nums)
         {
+            int res = nums[0];
 
+            int left = 0, right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                if (nums[left] < nums[right])
+                {
+                    res = Math.Min(res, nums[left]);
+                    break;
+                }
+
+                int middle = (left + right) / 2;
+                res = Math.Min(res, nums[middle]);
+
+                if (nums[middle] >= nums[left])
+                    left = middle + 1;
+                else
+                    right = middle - 1;
+            }
+
+            return res;
         }
     }
 }
