45ef34f19b0461fdfb33cf7f6d1cadfbe3b0aa12
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-08 08:54:07 +0330

    solve binary tree level order traversal problem

---
+++ Trees/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversalProblem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Trees.BinaryTreeLevelOrderTraversal
+{
+    public static class BinaryTreeLevelOrderTraversalProblem
+    {
+        public static IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            List<List<int>> res = new();
+
+            Queue<TreeNode> queue = new(new List<TreeNode>() { root });
+
+            while (queue.Count > 0)
+            {
+                List<int> nodes = new();
+
+                foreach (var i in Enumerable.Range(1, queue.Count))
+                {
+                    var node = queue.Dequeue();
+
+                    if (node is not null)
+                    {
+                        nodes.Add(node.val);
+
+                        if (node.left is not null)
+                            queue.Enqueue(node.left);
+
+                        if (node.right is not null)
+                            queue.Enqueue(node.right);
+                    }
+                }
+
+                if (nodes.Any())
+                    res.Add(nodes);
+            }
+
+            return res.ToArray();
+        }
+    }
+}
--- Trees/Program.cs
+++ Trees/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using Trees.BinaryTreeLevelOrderTraversal;
 using Trees.MaximumDepthOfBinaryTree;
+
+//var traverse = BinaryTreeLevelOrderTraversalProblem.LevelOrder(new(3, new(9), new(20, new(15), new(7))));
+var traverse = BinaryTreeLevelOrderTraversalProblem.LevelOrder(null);
 
 //InvertBinaryTreeProblem.InvertTree(new(4, new(2, new(1), new(3)), new(7, new(6), new(9))));
 
-int depth = MaximumDepthOfBinaryTreeProblem.MaxDepth(new(3, new(9), new(20, new(15), new(7))));
+//int depth = MaximumDepthOfBinaryTreeProblem.MaxDepth(new(3, new(9), new(20, new(15), new(7))));
 
-Console.WriteLine(depth);
+Console.WriteLine(string.Empty);
