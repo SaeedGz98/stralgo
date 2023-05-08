afa61e4307c455b15540e5881d01d28e970e8a7e
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-08 12:22:09 +0330

    solve validate binary search tree problem

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using Trees.BinaryTreeLevelOrderTraversal;
 using Trees.MaximumDepthOfBinaryTree;
+using Trees.ValidateBinarySearchTree;
 
-//var traverse = BinaryTreeLevelOrderTraversalProblem.LevelOrder(new(3, new(9), new(20, new(15), new(7))));
-var traverse = BinaryTreeLevelOrderTraversalProblem.LevelOrder(null);
+//var isValidBst = ValidateBinarySearchTreeProblem.IsValidBST(new(2, new(1), new(3)));
+var isValidBst = ValidateBinarySearchTreeProblem.IsValidBST(new(5, new(1), new(4, new(3), new(6))));
 
-//InvertBinaryTreeProblem.InvertTree(new(4, new(2, new(1), new(3)), new(7, new(6), new(9))));
-
-//int depth = MaximumDepthOfBinaryTreeProblem.MaxDepth(new(3, new(9), new(20, new(15), new(7))));
-
-Console.WriteLine(string.Empty);
+Console.WriteLine(isValidBst);
---
+++ Trees/ValidateBinarySearchTree/ValidateBinarySearchTreeProblem.cs
@@ -0,0 +1,21 @@
+﻿namespace Trees.ValidateBinarySearchTree
+{
+    public static class ValidateBinarySearchTreeProblem
+    {
+        public static bool IsValidBST(TreeNode root)
+        {
+            return IsValid(root, long.MinValue, long.MaxValue);
+        }
+
+        public static bool IsValid(TreeNode node, int left, int right)
+        {
+            if (node is null)
+                return true;
+
+            if (!(node.val > left && node.val < right))
+                return false;
+
+            return IsValid(node.left, left, node.val) && IsValid(node.right, node.val, right);
+        }
+    }
+}
