5d4f57feea35f5630507dfaeb0c2bd0b34368715
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-09 11:49:54 +0330

    solve construct binary tree from pre order and inorder traversal problem

---
+++ Trees/ConstructBinaryTreeFromPreorderAndInorderTraversal/ConstructBinaryTreeFromPreorderAndInorderTraversalProblem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Trees.ConstructBinaryTreeFromPreorderAndInorderTraversal
+{
+    public static class ConstructBinaryTreeFromPreorderAndInorderTraversalProblem
+    {
+        public static TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            if (preorder is null || preorder.Length == 0 || inorder is null || inorder.Length == 0)
+                return null;
+
+            TreeNode root = new(preorder[0]);
+            int mid = Array.IndexOf(inorder, preorder[0]);
+            root.left = BuildTree(preorder[1..(mid + 1)], inorder[..mid]);
+            root.right = BuildTree(preorder[(mid + 1)..], inorder[(mid + 1)..]);
+
+            return root;
+        }
+    }
+}
