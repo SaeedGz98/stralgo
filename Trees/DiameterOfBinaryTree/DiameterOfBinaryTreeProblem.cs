3b35beb530a009f5b7dc9402aa1f5857a541d04e
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-10 13:49:50 +0330

    solve diameter of binary tree problem

---
+++ Trees/DiameterOfBinaryTree/DiameterOfBinaryTreeProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Trees.DiameterOfBinaryTree
+{
+    public class DiameterOfBinaryTreeProblem
+    {
+        private int result = -1;
+
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            Dfs(root);
+            return result;
+        }
+
+        private int Dfs(TreeNode current)
+        {
+            if (current == null)
+            {
+                return -1;
+            }
+            int left = 1 + Dfs(current.left);
+            int right = 1 + Dfs(current.right);
+            result = Math.Max(result, (left + right));
+            return Math.Max(left, right);
+        }
+    }
+}
