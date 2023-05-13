027424b6e49d6685233509b3a8ce40381914f06e
Saeed <saeed_prime@outlook.com>
2023-05-13 23:58:02 +0330

    add subset with dup

--- Backtracking/Program.cs
+++ Backtracking/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Backtracking.Subsets;
 using System.Collections.Generic;
 
+var result = SubsetsProblem.Subsets(new int[] { 1, 2, 3 });
 IList<IList<int>> subsets = SubsetsProblem.Subsets(new int[] { 1, 2, 3 });
---
+++ Backtracking/SubsetsII/SubsetsIIProblem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backtracking.SubsetsII
+{
+    public static class SubsetsIIProblem
+    {
+        public static IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
