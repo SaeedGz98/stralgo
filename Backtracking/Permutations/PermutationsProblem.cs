8b2fe586c0503ba5c429e99133a55c2d001bd3c4
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-13 16:57:50 +0330

    solve permutations problem

---
+++ Backtracking/Permutations/PermutationsProblem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Backtracking.Permutations
+{
+    public record class PermutationsProblem
+    {
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            List<List<int>> result = new();
+
+            if (nums.Length == 1)
+                return new List<List<int>>() { nums.ToList() }.ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int n = nums[0];
+                nums = nums[1..];
+
+                IList<IList<int>> perms = Permute(nums);
+
+                foreach (var perm in perms)
+                    perm.Add(n);
+
+                result.AddRange(perms.Select(x => x.ToList()));
+                nums = nums.Append(n).ToArray();
+            }
+
+            return result.ToArray();
+        }
+    }
+}
