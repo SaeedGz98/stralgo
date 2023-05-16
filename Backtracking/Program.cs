e3930427b9c7ac3aa907e5b18c537e711d163b38
Saeed <saeed_prime@outlook.com>
2023-05-16 11:22:48 +0330

    solve subsets ii problem

--- Backtracking/Program.cs
+++ Backtracking/Program.cs
@@ -1,5 +1,5 @@
-﻿using Backtracking.Subsets;
+﻿using Backtracking.SubsetsII;
 using System.Collections.Generic;
 
-var result = SubsetsProblem.Subsets(new int[] { 1, 2, 3 });
-IList<IList<int>> subsets = SubsetsProblem.Subsets(new int[] { 1, 2, 3 });
+IList<IList<int>> result = SubsetsIIProblem.SubsetsWithDup(new int[] { 1, 2, 2, 3 });
+var a = 2;
--- Backtracking/SubsetsII/SubsetsIIProblem.cs
+++ Backtracking/SubsetsII/SubsetsIIProblem.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Backtracking.SubsetsII
 {
@@ -7,7 +7,32 @@ namespace Backtracking.SubsetsII
     {
         public static IList<IList<int>> SubsetsWithDup(int[] nums)
         {
-            throw new NotImplementedException();
+            List<List<int>> res = new();
+            nums = nums.OrderBy(x => x).ToArray();
+
+            void BackTrack(int i, Stack<int> subset)
+            {
+                if (i == nums.Length)
+                {
+                    res.Add(subset.ToList());
+                    return;
+                }
+
+                //All subsets that include nums[i]
+                subset.Push(nums[i]);
+                BackTrack(i + 1, subset);
+                subset.Pop();
+
+                //All subsets that don't include nums[i]
+                while (i + 1 < nums.Length && nums[i] == nums[i + 1])
+                    i += 1;
+
+                BackTrack(i + 1, subset);
+            }
+
+            BackTrack(0, new());
+
+            return res.ToArray();
         }
     }
 }
