b8c74f415cc959f11b257c8bb3f0e46caea76557
Saeed <saeed_prime@outlook.com>
2023-05-19 12:31:02 +0330

    solve kth largest with heap problem

---
+++ Heap_PriorityQueue/KthLargestElementInAStream/KthLargestElementInAStreamProblem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Heap_PriorityQueue.KthLargestElementInAStream
+{
+    public class KthLargest
+    {
+        private readonly PriorityQueue<int, int> _heap = new();
+        private readonly int _k;
+
+        public KthLargest(int k, int[] nums)
+        {
+            _k = k;
+            foreach (var num in nums)
+            {
+                Add(num);
+            }
+        }
+
+        public int Add(int val)
+        {
+            _heap.Enqueue(val, val);
+
+            if (_heap.Count > _k)
+                _heap.Dequeue();
+
+            return _heap.Peek();
+        }
+    }
+}
