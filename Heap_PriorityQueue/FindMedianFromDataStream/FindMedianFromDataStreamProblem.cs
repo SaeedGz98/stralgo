502d1182088ac4740f6f16ff3f85754d4ed29964
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-21 11:09:25 +0330

    solve find median from data stream problem

---
+++ Heap_PriorityQueue/FindMedianFromDataStream/FindMedianFromDataStreamProblem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Heap_PriorityQueue.FindMedianFromDataStream
+{
+    public class MedianFinder
+    {
+        private PriorityQueue<int, int> _small;
+        private PriorityQueue<int, int> _large;
+
+        public MedianFinder()
+        {
+            _small = new(Comparer<int>.Create((x, y) => y - x));
+            _large = new();
+        }
+
+        public void AddNum(int num)
+        {
+            _small.Enqueue(num, num);
+
+            if (_small.Count > 0 && _large.Count > 0 && _small.Peek() > _large.Peek())
+            {
+                int val = _small.Dequeue();
+                _large.Enqueue(val, val);
+            }
+
+            if (_small.Count > _large.Count + 1)
+            {
+                int val = _small.Dequeue();
+                _large.Enqueue(val, val);
+            }
+
+            if (_large.Count > _small.Count + 1)
+            {
+                int val = _large.Dequeue();
+                _small.Enqueue(val, val);
+            }
+        }
+
+        public double FindMedian()
+        {
+            if (_small.Count > _large.Count)
+                return _small.Peek();
+            else if (_large.Count > _small.Count)
+                return _large.Peek();
+            else
+                return (double)((decimal)(_small.Peek() + _large.Peek()) / 2);
+        }
+    }
+}
--- Heap_PriorityQueue/Program.cs
+++ Heap_PriorityQueue/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Heap_PriorityQueue.FindMedianFromDataStream;
+using System;
+
+var finder = new MedianFinder();
+finder.AddNum(1);
+finder.AddNum(2);
+Console.WriteLine(finder.FindMedian());
+finder.AddNum(3);
+Console.WriteLine(finder.FindMedian());
+Console.ReadLine();
