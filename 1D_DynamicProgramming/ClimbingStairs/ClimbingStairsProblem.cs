71de4911af3b8ff34d8fcced596c1043e78014c7
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-22 09:33:44 +0330

    solve climbing stairs problem

--- 1D_DynamicProgramming/ClimbingStairs/ClimbingStairsProblem.cs
+++ 1D_DynamicProgramming/ClimbingStairs/ClimbingStairsProblem.cs
@@ -1,13 +1,17 @@
-﻿namespace _1D_DynamicProgramming.ClimbingStairs
+﻿using System.Linq;
+
+namespace _1D_DynamicProgramming.ClimbingStairs
 {
     public static class ClimbingStairsProblem
     {
         public static int ClimbStairs(int n)
         {
+            int one = 1, two = 1;
 
+            foreach (var item in Enumerable.Range(0, n - 1))
+                (one, two) = (one + two, one);
 
-
-            return default;
+            return one;
         }
     }
 }
--- 1D_DynamicProgramming/Program.cs
+++ 1D_DynamicProgramming/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _1D_DynamicProgramming.ClimbingStairs;
+using System;
+
+var result = ClimbingStairsProblem.ClimbStairs(5);
+Console.WriteLine(result);
