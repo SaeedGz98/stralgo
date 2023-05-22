7d4029ada5cc249e6e56b161c25c43fa9670d47a
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-22 15:57:25 +0330

    solve house robber problem

---
+++ 1D_DynamicProgramming/HouseRobber/HouseRobberProblem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _1D_DynamicProgramming.HouseRobber
+{
+    public static class HouseRobberProblem
+    {
+        public int Rob(int[] nums)
+        {
+            int rob1 = 0, rob2 = 0;
+
+            foreach (var num in nums)
+            {
+                (rob1, rob2) = (rob2, Math.Max(num + rob1, rob2));
+            };
+
+            return rob2;
+        }
+    }
+}
