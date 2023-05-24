947611e5abc487079e759eec7768f70d7e554d9b
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-24 10:52:22 +0330

    solve coin change problem

--- 1D_DynamicProgramming/CoinChange/CoinChangeProblem.cs
+++ 1D_DynamicProgramming/CoinChange/CoinChangeProblem.cs
@@ -1,12 +1,27 @@
-﻿namespace _1D_DynamicProgramming.CoinChange
+﻿using System;
+using System.Linq;
+
+namespace _1D_DynamicProgramming.CoinChange
 {
     public static class CoinChangeProblem
     {
         public static int CoinChange(int[] coins, int amount)
         {
+            int[] dp = Enumerable.Repeat(amount + 1, amount + 1).ToArray();
+            dp[0] = 0;
 
+            foreach (var coin in coins)
+            {
+                for (int num = 1; num < amount + 1; num++)
+                {
+                    if (num >= coin)
+                    {
+                        dp[num] = Math.Min(dp[num], 1 + dp[num - coin]);
+                    }
+                }
+            }
 
-            return default;
+            return dp[amount] != amount + 1 ? dp[amount] : -1;
         }
     }
 }
--- 1D_DynamicProgramming/Program.cs
+++ 1D_DynamicProgramming/Program.cs
@@ -1,5 +1,5 @@
-﻿using _1D_DynamicProgramming.ClimbingStairs;
+﻿using _1D_DynamicProgramming.CoinChange;
 using System;
 
-var result = ClimbingStairsProblem.ClimbStairs(5);
+var result = CoinChangeProblem.CoinChange(new int[] { 1, 3, 4, 5 }, 7);
 Console.WriteLine(result);
