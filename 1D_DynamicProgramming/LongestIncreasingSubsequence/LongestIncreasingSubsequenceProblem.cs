5532a4f3574fb4c7b5b72d8e7f58b06a3ec3fabe
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-27 09:15:45 +0330

    solve length of lis problem

---
+++ 1D_DynamicProgramming/LongestIncreasingSubsequence/LongestIncreasingSubsequenceProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _1D_DynamicProgramming.LongestIncreasingSubsequence
+{
+    public static class LongestIncreasingSubsequenceProblem
+    {
+        public static int LengthOfLIS(int[] nums)
+        {
+            int[] lis = Enumerable.Repeat(1, nums.Length).ToArray();
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] > nums[i])
+                    {
+                        lis[i] = Math.Max(lis[i], 1 + lis[j]);
+                    }
+                }
+            }
+
+            return lis.Max();
+        }
+    }
+}
