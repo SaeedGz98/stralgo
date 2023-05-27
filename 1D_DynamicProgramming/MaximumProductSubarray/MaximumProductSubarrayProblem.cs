6e9e73ff6c30a5d968c907022938f13b96a245b7
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-27 12:22:12 +0330

    solve max product subarray problem

---
+++ 1D_DynamicProgramming/MaximumProductSubarray/MaximumProductSubarrayProblem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _1D_DynamicProgramming.MaximumProductSubarray
+{
+    public static class MaximumProductSubarrayProblem
+    {
+        public static int MaxProduct(int[] nums)
+        {
+            int res = nums.Max();
+            int curMin = 1, curMax = 1;
+
+            foreach (int num in nums)
+            {
+                int tmp = num * curMax;
+                curMax = new int[] { num * curMax, num * curMin, num }.Max();
+                curMin = new int[] { tmp, num * curMin, num }.Min();
+
+                res = Math.Max(res, curMax);
+            }
+
+            return res;
+        }
+    }
+}
