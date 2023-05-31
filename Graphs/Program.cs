a7578127957bd957a9b30a285bd4ff1323960a74
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-05-31 11:27:10 +0330

    solve rotting oranges problem

--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -1,16 +1,4 @@
-﻿using Graphs.MaxAreaOfIsland;
-using Graphs.NumberOfIslands;
+﻿using Graphs.RottingOranges;
 
-var result = MaxAreaOfIslandProblem.MaxAreaOfIsland(new int[][]
-{
-    new int[] { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
-    new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-    new int[] { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
-    new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
-    new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
-    new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
-    new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-    new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
-});
-
+var result = RottingOrangesProblem.OrangesRotting(new int[][] { new int[] { 2, 1, 1 }, new int[] { 1, 1, 0 }, new int[] { 0, 1, 1 } });
 Console.WriteLine(result);
---
+++ Graphs/RottingOranges/RottingOrangesProblem.cs
@@ -0,0 +1,51 @@
+﻿namespace Graphs.RottingOranges
+{
+    public static class RottingOrangesProblem
+    {
+        public static int OrangesRotting(int[][] grid)
+        {
+            int rows = grid.Length, cols = grid[0].Length;
+            Queue<(int r, int c)> queue = new();
+            int times = 0, fresh = 0;
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if (grid[r][c] == 1)
+                        fresh++;
+
+                    if (grid[r][c] == 2)
+                        queue.Enqueue((r, c));
+                }
+            }
+
+            (int dr, int dc)[] directions = { (1, 0), (0, 1), (-1, 0), (0, -1) };
+
+            while (queue.Count > 0 && fresh > 0)
+            {
+                foreach (var i in Enumerable.Range(0, queue.Count))
+                {
+                    (int r, int c) = queue.Dequeue();
+
+                    foreach ((int dr, int dc) in directions)
+                    {
+                        int row = r + dr;
+                        int col = c + dc;
+
+                        if (row < 0 || col < 0 || row == rows || col == cols && grid[row][col] != 1)
+                            continue;
+
+                        fresh--;
+                        grid[row][col] = 2;
+                        queue.Enqueue((row, col));
+                    }
+                }
+
+                times++;
+            }
+
+            return fresh == 0 ? times : -1;
+        }
+    }
+}
