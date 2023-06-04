55b678e1b502b048274e8510105ff255af3c17d6
Saeed <saeed_prime@outlook.com>
2023-06-04 23:46:08 +0330

    solve max sub array

---
+++ Greedy/MaximumSubarray/MaximumSubarrayProblem.cs
@@ -0,0 +1,22 @@
+﻿namespace Greedy.MaximumSubarray
+{
+    public static class MaximumSubarrayProblem
+    {
+        public static int MaxSubArray(int[] nums)
+        {
+            int maxSub = nums[0];
+            int curSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (curSum < 0)
+                {
+                    curSum = 0;
+                }
+                curSum += nums[i];
+                maxSub = Math.Max(maxSub, curSum);
+            }
+            return maxSub;
+        }
+    }
+}
