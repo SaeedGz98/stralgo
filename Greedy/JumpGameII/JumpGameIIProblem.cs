dc0838558e9840e57ed3c7fd99c6e1a947be5fd1
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-06-06 13:07:56 +0330

    solve jump game ii problem

---
+++ Greedy/JumpGameII/JumpGameIIProblem.cs
@@ -0,0 +1,25 @@
+﻿namespace Greedy.JumpGameII
+{
+    public static class JumpGameIIProblem
+    {
+        public static int Jump(int[] nums)
+        {
+            int res = 0;
+            int l = 0, r = 0;
+
+            while (r < nums.Length - 1)
+            {
+                int farthest = 0;
+
+                foreach (int i in Enumerable.Range(l, r))
+                    farthest = Math.Max(farthest, i + nums[i]);
+
+                l = r + 1;
+                r = farthest;
+                res++;
+            }
+
+            return res;
+        }
+    }
+}
