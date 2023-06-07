756b6a1cdbb58bce55921a36bff48ad2147f95c6
Saeed <saeed_prime@outlook.com>
2023-06-07 19:50:48 +0330

    solve gas station problem

---
+++ Greedy/GasStation/GasStationProblem.cs
@@ -0,0 +1,19 @@
+﻿namespace Greedy.GasStation
+{
+    public static class GasStationProblem
+    {
+        public static int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            if (cost.Sum() > gas.Sum())
+                return -1;
+
+            int total = 0;
+            int res = 0;
+
+            for (int i = 0; i < gas.Length; i++)
+            {
+                total += (gas[i] - cost[i]);
+            }
+        }
+    }
+}
