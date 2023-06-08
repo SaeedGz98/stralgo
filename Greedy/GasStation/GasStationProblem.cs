ae2836adc09e1c3720939950a38909126d160f1e
Saeed <saeed_prime@outlook.com>
2023-06-08 09:27:55 +0330

    fix gas station problem

--- Greedy/GasStation/GasStationProblem.cs
+++ Greedy/GasStation/GasStationProblem.cs
@@ -13,7 +13,15 @@
             for (int i = 0; i < gas.Length; i++)
             {
                 total += (gas[i] - cost[i]);
+
+                if (total < 0)
+                {
+                    total = 0;
+                    res = i + 1;
+                }
             }
+
+            return res;
         }
     }
 }
