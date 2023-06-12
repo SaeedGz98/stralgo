ed440e2925ff6e8cb3cf5b0df3d3dc14df225937
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-06-12 09:02:21 +0330

    solve unique path problem

---
+++ 2D_DynamicProgramming/UniquePaths/UniquePathsProblem.cs
@@ -0,0 +1,24 @@
+﻿namespace _2D_DynamicProgramming.UniquePaths
+{
+    public static class UniquePathsProblem
+    {
+        public static int UniquePaths(int m, int n)
+        {
+            int[] row = Enumerable.Repeat(1, n).ToArray();
+
+            for (int i = 0; i < m - 1; i++)
+            {
+                int[] newRow = Enumerable.Repeat(1, n).ToArray();
+
+                for (int j = n - 2; j >= 0; j--)
+                {
+                    newRow[j] = newRow[j + 1] + row[j];
+                }
+
+                row = newRow;
+            }
+
+            return row[0];
+        }
+    }
+}
