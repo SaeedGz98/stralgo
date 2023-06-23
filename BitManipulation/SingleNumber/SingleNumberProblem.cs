23bcb541f3ed62eea3d8374934488aa113f8fe8b
Saeed <saeed_prime@outlook.com>
2023-06-23 11:22:24 +0330

    solve single number problem

---
+++ BitManipulation/SingleNumber/SingleNumberProblem.cs
@@ -0,0 +1,15 @@
+﻿namespace BitManipulation.SingleNumber
+{
+    public class SingleNumberProblem
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            int res = 0;
+
+            foreach (var num in nums)
+                res = res ^ num;
+
+            return res;
+        }
+    }
+}
