05a5fcecf6a7be4c0609792d9055e07996c399d5
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-06-25 13:37:16 +0330

    solve counting bit problem

---
+++ BitManipulation/CountingBits/CountingBitsProblem.cs
@@ -0,0 +1,21 @@
+﻿namespace BitManipulation.CountingBits
+{
+    public class CountingBitsProblem
+    {
+        public static int[] CountBits(int n)
+        {
+            int[] dp = Enumerable.Repeat(0, n + 1).ToArray();
+            int offset = 1;
+
+            for (var i = 1; i < n + 1; i++)
+            {
+                if (offset * 2 == i)
+                    offset = i;
+
+                dp[i] = 1 + dp[i - offset];
+            }
+
+            return dp;
+        }
+    }
+}
