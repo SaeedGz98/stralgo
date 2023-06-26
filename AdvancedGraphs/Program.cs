ace289070170522580da31b2392e6488f4c6d1e4
saeed.gz <saeed.gholamzadeh198@gmail.com>
2023-06-26 13:44:05 +0330

    solve network delay problem

--- AdvancedGraphs/Program.cs
+++ AdvancedGraphs/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdvancedGraphs.NetworkDelayTime;
+
+NetworkDelayTimeProblem.NetworkDelayTime(new int[][] { new int[] { 1, 2, 4 }, new int[] { 1, 3, 1 }, new int[] { 3, 4, 1 }, new int[] { 4, 2, 1 } }, 4, 1);
