6bccdcd76e2febb52ba7ead79148d9a0526cf51d
Saeed gholamzadeh <saeed.gholamzadeh198@gmail.com>
2024-07-12 10:20:09 +0330

    solve best time to buy and sell stock

---
+++ SlidingWindow/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SlidingWindow.BestTimeToBuyAndSellStock
+{
+    /// RECOMMENDED
+    public static class BestTimeToBuyAndSellStockProblem
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int left = 0, right = 1;
+            int maxProfit = 0;
+
+            while (right < prices.Length)
+            {
+                if (prices[left] < prices[right])
+                {
+                    int profit = prices[right] - prices[left];
+                    maxProfit = Math.Max(maxProfit, profit);
+                }
+                else
+                {
+                    left = right;
+                }
+
+                right++;
+            }
+
+            return maxProfit;
+        }
+    }
+}
--- SlidingWindow/Program.cs
+++ SlidingWindow/Program.cs
@@ -1,3 +1,6 @@
-﻿using System;
+﻿using SlidingWindow.BestTimeToBuyAndSellStock;
+using System;
+
+int result = BestTimeToBuyAndSellStockProblem.MaxProfit([7, 1, 5, 3, 6, 4, 0, 2]);
 
 Console.WriteLine("Hi");
