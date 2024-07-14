340485294874e5bc70f004c26c322bb60c2234b2
Saeed gholamzadeh <saeed.gholamzadeh198@gmail.com>
2024-07-14 05:52:22 +0330

    solve maximum product subarray problem

--- 1D_DynamicProgramming/MaximumProductSubarray/MaximumProductSubarrayProblem.cs
+++ 1D_DynamicProgramming/MaximumProductSubarray/MaximumProductSubarrayProblem.cs
@@ -3,23 +3,45 @@ using System.Linq;
 
 namespace _1D_DynamicProgramming.MaximumProductSubarray
 {
+    /// RECOMMENDED
     public static class MaximumProductSubarrayProblem
     {
         public static int MaxProduct(int[] nums)
         {
-            int res = nums.Max();
-            int curMin = 1, curMax = 1;
+            double res = nums[0];
+            double curMin = 1, curMax = 1;
 
             foreach (int num in nums)
             {
-                int tmp = num * curMax;
-                curMax = new int[] { num * curMax, num * curMin, num }.Max();
-                curMin = new int[] { tmp, num * curMin, num }.Min();
+                double tmp = num * curMax;
+                curMax = Math.Max(Math.Max(num * curMax, num * curMin), num);
+                curMin = Math.Min(Math.Min(tmp, num * curMin), num);
 
                 res = Math.Max(res, curMax);
             }
 
-            return res;
+            return (int)res;
+        }
+
+        public static int MaxProduct2(int[] nums)
+        {
+            int length = nums.Length;
+            double leftProduct = 1;
+            double rightProduct = 1;
+            double asnwer = nums[0];
+
+            for (int i = 0; i < length; i++)
+            {
+                leftProduct = leftProduct == 0 ? 1 : leftProduct;
+                rightProduct = rightProduct == 0 ? 1 : rightProduct;
+
+                leftProduct *= nums[i];
+                rightProduct *= nums[^(i + 1)];
+
+                asnwer = Math.Max(asnwer, Math.Max(leftProduct, rightProduct));
+            }
+
+            return (int)asnwer;
         }
     }
 }
--- 1D_DynamicProgramming/Program.cs
+++ 1D_DynamicProgramming/Program.cs
@@ -1,6 +1,9 @@
-﻿using _1D_DynamicProgramming.MaximumSubarray;
+﻿using _1D_DynamicProgramming.MaximumProductSubarray;
+using _1D_DynamicProgramming.MaximumSubarray;
 using System;
 
-var result = MaximumSubarrayProblem.MaxSubArray([-2, -1, -3]);
+int result = MaximumSubarrayProblem.MaxSubArray([-2, -1, -3]);
+int result2 = MaximumProductSubarrayProblem.MaxProduct([0, 10, 10, 10, 10, 10, 10, 10, 10, 10, -10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0]);
+int result3 = MaximumProductSubarrayProblem.MaxProduct2([0, 10, 10, 10, 10, 10, 10, 10, 10, 10, -10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0]);
 
 Console.WriteLine(result);
