c59077e19a79bc387553fc2a13997052a2ef0b29
Saeed gholamzadeh <saeed.gholamzadeh198@gmail.com>
2024-07-16 08:23:34 +0330

    solve max sliding windows

--- SlidingWindow/Program.cs
+++ SlidingWindow/Program.cs
@@ -1,6 +1,8 @@
 ﻿using SlidingWindow.BestTimeToBuyAndSellStock;
+using SlidingWindow.SlidingWindowMaximum;
 using System;
 
 int result = BestTimeToBuyAndSellStockProblem.MaxProfit([7, 1, 5, 3, 6, 4, 0, 2]);
+int[] result2 = SlidingWindowMaximumProblem.MaxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7],3);
 
 Console.WriteLine("Hi");
---
+++ SlidingWindow/SlidingWindowMaximum/SlidingWindowMaximumProblem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace SlidingWindow.SlidingWindowMaximum
+{
+    public class SlidingWindowMaximumProblem
+    {
+        /// RECOMMENDED
+        public static int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0 || k == 0) return [];
+
+            List<int> output = [];
+            LinkedList<int> deque = new();
+            int l = 0, r = 0;
+
+            while (r < nums.Length)
+            {
+                // Pop smaller values from deque
+                while (deque.Count > 0 && nums[deque.Last.Value] < nums[r])
+                {
+                    deque.RemoveLast();
+                }
+                deque.AddLast(r);
+
+                // Remove left value from window
+                if (l > deque.First.Value)
+                {
+                    deque.RemoveFirst();
+                }
+
+                // If window has hit size k, add to results
+                if (r + 1 >= k)
+                {
+                    output.Add(nums[deque.First.Value]);
+                    l++;
+                }
+
+                r++;
+            }
+
+            return output.ToArray();
+        }
+    }
+}
