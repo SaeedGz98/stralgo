9382711c5fd8a38045e3d215a02e6a23f659bc37
Saeed gholamzadeh <saeed.gholamzadeh198@gmail.com>
2024-07-17 16:30:22 +0330

    solve valid anagram problem

--- Arrays_Hashing/Program.cs
+++ Arrays_Hashing/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Arrays_Hashing.ContainsDuplicate;
 using Arrays_Hashing.ProductExceptSelf;
 using Arrays_Hashing.TwoSum;
+using Arrays_Hashing.ValidAnagram;
 
 int[] result = TwoSumProblem.TwoSum([2, 7, 11, 15], 9);
 int[] result2 = ProductExceptSelfProblem.ProductExceptSelf([1, 2, 3, 4]);
 bool result3 = ContainsDuplicateProblem.ContainsDuplicate([1, 2, 3, 1]);
+bool result4 = ValidAnagramProblem.IsAnagram("anagram", "nagaram");
+bool result5 = ValidAnagramProblem.IsAnagram2("rat", "car");
 
 Console.ReadLine();
--- Arrays_Hashing/ValidAnagram/ValidAnagramProblem.cs
+++ Arrays_Hashing/ValidAnagram/ValidAnagramProblem.cs
@@ -1,15 +1,13 @@
-﻿using System.Linq;
-
-namespace Arrays_Hashing.ValidAnagram
+﻿namespace Arrays_Hashing.ValidAnagram
 {
+    /// RECOMMENDED
     public static class ValidAnagramProblem
     {
         public static bool IsAnagram(string s, string t)
         {
             if (s.Length != t.Length) return false;
 
-
-            Dictionary<char, int> hashS = new(), hashT = new();
+            Dictionary<char, int> hashS = [], hashT = [];
 
             for (int i = 0; i < s.Length; i++)
             {
@@ -26,6 +24,21 @@ namespace Arrays_Hashing.ValidAnagram
             }
 
             return true;
+        }
+
+        public static bool IsAnagram2(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+
+            int[] chartCounts = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                chartCounts[s[i] - 'a']++;
+                chartCounts[t[i] - 'a']--;
+            }
+
+            return chartCounts.All(x => x == 0);
         }
     }
 }
