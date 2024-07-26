4339f88c47856c13e498382bf777897cbd8bf839
Saeed gholamzadeh <saeed.gholamzadeh198@gmail.com>
2024-07-26 13:00:08 +0330

    solve spiral matrix problem

--- Math&Geometry/Program.cs
+++ Math&Geometry/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Math_Geometry.SetMatrixZeroes;
+using Math_Geometry.SpiralMatrix;
 
 SetMatrixZeroesProblem.SetZeroes([[1, 1, 1], [1, 0, 1], [1, 1, 1]]);
+IList<int> result = SpiralMatrixProblem.SpiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
 
 Console.WriteLine("|| Math_Geometry ||");
---
+++ Math&Geometry/SpiralMatrix/SpiralMatrixProblem.cs
@@ -0,0 +1,43 @@
+﻿namespace Math_Geometry.SpiralMatrix
+{
+    /// <summary>
+    /// RECOMMENDED
+    /// </summary>
+    public class SpiralMatrixProblem
+    {
+        public static IList<int> SpiralOrder(int[][] matrix)
+        {
+            List<int> res = [];
+            int left = 0, right = matrix[0].Length;
+            int top = 0, bottom = matrix.Length;
+
+            while (left < right && top < bottom)
+            {
+                for (int i = left; i < right; i++)
+                    res.Add(matrix[top][i]);
+
+                top++;
+
+                for (int i = top; i < bottom; i++)
+                    res.Add(matrix[i][right - 1]);
+
+                right--;
+
+                if (!(left < right && top < bottom))
+                    break;
+
+                for (int i = right - 1; i >= left; i--)
+                    res.Add(matrix[bottom - 1][i]);
+
+                bottom--;
+
+                for (int i = bottom - 1; i >= top; i--)
+                    res.Add(matrix[i][left]);
+
+                left++;
+            }
+
+            return res;
+        }
+    }
+}
